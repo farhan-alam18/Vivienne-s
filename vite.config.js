2fe030ced289da14db42be37592e9691905a2414
farhan-alam18 <farhanalam1113@gmail.com>
2023-10-24 09:38:40 +0530

    Package json changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production'
+  ? 'https://farhan-alam18.github.io/Vivienne-s'
+  : '/'
 })
